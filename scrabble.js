888242502b0c6444b48d0a7ac0fcdb7d130f3f76
Allison Hoke <allisonhoke@gmail.com>
2016-11-16 11:40:25 -0800

    score function written and working

--- scrabble.js
+++ scrabble.js
@@ -1,34 +1,52 @@
-var Scrabble = function() {};
 
-var LetterValue = {
-  a: 1,
-  b: 3,
-  c: 3,
-  d: 2,
-  e: 1,
-  f: 4,
-  g: 2,
-  h: 4,
-  i: 1,
-  j: 8,
-  k: 5,
-  l: 1,
-  m: 3,
-  n: 1,
-  o: 1,
-  p: 3,
-  q: 10,
-  r: 1,
-  s: 1,
-  t: 1,
-  u: 1,
-  v: 4,
-  w: 4,
-  x: 8,
-  y: 4,
-  z: 10
+
+var Scrabble = function() {
+  this.letterValues = {
+    a: 1,
+    b: 3,
+    c: 3,
+    d: 2,
+    e: 1,
+    f: 4,
+    g: 2,
+    h: 4,
+    i: 1,
+    j: 8,
+    k: 5,
+    l: 1,
+    m: 3,
+    n: 1,
+    o: 1,
+    p: 3,
+    q: 10,
+    r: 1,
+    s: 1,
+    t: 1,
+    u: 1,
+    v: 4,
+    w: 4,
+    x: 8,
+    y: 4,
+    z: 10
+  };
 };
 
+Scrabble.prototype.score = function(word) {
+  var wordLetters = word.split("");
+  var lettersLength = word.length;
+  var valueOfLetters = this.letterValues;
+  var total = 0;
+
+  wordLetters.forEach(function(letter) {
+    var letterScore = valueOfLetters[letter];
+    total += letterScore;
+  });
+  return total;
+};
+
+var newScrabble = new Scrabble();
+  var testWord = newScrabble.score("allison");
+  console.log(testWord);
 
 
 
