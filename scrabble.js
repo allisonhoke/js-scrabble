ef5e2794d67743c3a0d2a11aa4270d04b16a495b
Allison Hoke <allisonhoke@gmail.com>
2016-11-16 12:28:09 -0800

    WIP highestScoreFrom function

--- scrabble.js
+++ scrabble.js
@@ -41,10 +41,49 @@ Scrabble.prototype.score = function(word) {
     var letterScore = valueOfLetters[letter];
     total += letterScore;
   });
+
+  if (lettersLength == 7) {
+    total += 50;
+  }
+
   return total;
 };
 
-// Scrabble.prototype.
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  var wordArray = arrayOfWords;
+  var arrayLength = wordArray.length;
+  var maxScore = 0;
+  var currentWord = [];
+
+  for(var i = 0; i < arrayLength; i++) {
+    var wordScore = this.score(wordArray[i]); //check the score for each word in the array
+    if (wordScore >= maxScore) {
+      if (wordScore > maxScore) {
+        currentWord = []; //reset the array because this word has a higher score than any that are in there
+        currentWord.push(wordArray[i]);
+        maxScore = wordScore;
+      } else {
+        currentWord.push(wordArray[i]) //add to the array if there is more than one word with this score
+      }
+    }
+  } // returns an array of words if there is a tie
+
+  //if current word length is 1, return that word, else do more logic
+
+  if (currentWord.length > 0) {
+    if (currentWord.length == 1) {
+      return currentWord[0]; //return the only word in the array
+    } else {
+
+    }
+  } else {
+    return "Something has gone horribly wrong.";
+  }
+console.log(maxScore);
+console.log(currentWord);
+
+
+};
 
 
 
@@ -59,6 +98,10 @@ var newScrabble = new Scrabble();
   var testWord = newScrabble.score("ALLISON");
   console.log(testWord);
 
+console.log("testing highestScoreFrom")
+  var newScrabble = new Scrabble();
+    var testArray = newScrabble.highestScoreFrom(["ALLISON", "yellow", "sand"]);
+    console.log(testArray);
 
 
 
